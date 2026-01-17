64e289c8e7b3220b2a1bf4f0c26edaa03000cfc2
dhorowitz001 <dlhorowitz@outlook.com>
2026-01-17 15:26:22 -0500

    Matching output

--- salon.sh
+++ salon.sh
@@ -4,20 +4,22 @@ MENU_RESULT=''
 while IFS=" | " read -r id name; do
   MENU_RESULT+="$id) $name\n"
 done < <($PSQL "SELECT service_id, name FROM services;")
-echo -e "~~~~~ MY SALON ~~~~~\n\nWelcome to My Salon, how can I help you?\n\n$MENU_RESULT"
+echo -e "~~~~~ MY SALON ~~~~~\n\nWelcome to My Salon, how can I help you?\n\n"
+printf "%b" "$MENU_RESULT"
 #prompt for service id, phone, name, time
 while true; do
-  echo -e "\nPlease enter your desired service:"
+#  echo -e "\nPlease enter your desired service:"
   read SERVICE_ID_SELECTED
   if [[ $MENU_RESULT == *"$SERVICE_ID_SELECTED)"* ]]; then
   SERVICE_RESULT=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
     break  # Valid service ID entered, exit the loop
   else
-    echo -e "I could not find that service. What would you like today?\n\n$MENU_RESULT"
+    echo -e "I could not find that service. What would you like today?\n\n"
+    printf "%b" "$MENU_RESULT"
   fi
 done
 while true; do
-  echo -e "\nPlease enter your phone number:"
+  echo -e "\nWhat's your phone number?"
   read CUSTOMER_PHONE
   PHONE_EXISTS=$($PSQL "SELECT COUNT(*) FROM customers WHERE phone = '$CUSTOMER_PHONE';" --no-align --tuples-only)
   if [ "$PHONE_EXISTS" -gt 0 ]; then
@@ -40,7 +42,7 @@ while true; do
 done
 CUSTOMER_NAME_RESULT=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE'")
 CUSTOMER_ID_RESULT=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'"  --tuples-only --no-align)
-echo -e "What time would you like your $SERVICE_RESULT, $CUSTOMER_NAME_RESULT?:"
+echo -e "\nWhat time would you like your $SERVICE_RESULT, $CUSTOMER_NAME_RESULT?:"
 read SERVICE_TIME
 while true; do
   TIME_PATTERN="^[0-9]{1,2}:[0-9]{2} ?(am|pm|AM|PM)?$"
@@ -52,6 +54,6 @@ while true; do
   fi
 done
 $PSQL "INSERT INTO appointments (customer_id, service_id, time) VALUES ($CUSTOMER_ID_RESULT, $SERVICE_ID_SELECTED, '$SERVICE_TIME')"
-echo -e "I have put you down for a $SERVICE_RESULT at $SERVICE_TIME, $CUSTOMER_NAME_RESULT."
+echo -e "\nI have put you down for a $SERVICE_RESULT at $SERVICE_TIME, $CUSTOMER_NAME_RESULT.\n"
 
 
